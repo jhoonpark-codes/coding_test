f7fdda9318826fc44d7d892d96b7f59220701352
Park Jung Hoon <154233920+jhoonpark-codes@users.noreply.github.com>
2024-01-12 11:28:16 +0900

    Update and rename 231206.py to 231206_1716.py

--- leetcode/daily_leetcode/231206_1716.py
+++ leetcode/daily_leetcode/231206_1716.py
@@ -1,4 +1,5 @@
 '''
+
 Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
 
 He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
