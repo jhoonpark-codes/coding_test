a2476b16b55c76bd1dc895923403d6de836565f4
Park Jung Hoon <154233920+jhoonpark-codes@users.noreply.github.com>
2024-01-15 13:48:25 +0900

    Update 240115_2225.py

--- leetcode/daily_leetcode/240115_2225.py
+++ leetcode/daily_leetcode/240115_2225.py
@@ -68,3 +68,26 @@ class Solution:
                 ans2.append(player)
         
         return [sorted(ans1), sorted(ans2)]
+
+'''
+best solution
+class Solution:
+    def findWinners(self, matches):
+        losses = [0] * 100001
+
+        for winner, loser in matches:
+            if losses[winner] == 0:
+                losses[winner] = -1
+
+            if losses[loser] == -1:
+                losses[loser] = 1
+            else:
+                losses[loser] += 1
+
+        zero_loss = [i for i in range(1, 100001) if losses[i] == -1]
+        one_loss = [i for i in range(1, 100001) if losses[i] == 1]
+
+        return [zero_loss, one_loss]
+
+'''
+
