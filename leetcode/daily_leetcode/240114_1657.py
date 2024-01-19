3bb389f16a34164e4411d86872f7d9b0aadd0985
Park Jung Hoon <154233920+jhoonpark-codes@users.noreply.github.com>
2024-01-19 12:23:24 +0900

    Update 240114_1657.py

--- leetcode/daily_leetcode/240114_1657.py
+++ leetcode/daily_leetcode/240114_1657.py
@@ -62,4 +62,21 @@ class Solution:
             
             else:
                 return False
-            
+
+
+'''
+Best solution -> 거의 똑같
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        frequency_word1 = Counter(word1)
+        frequency_word2 = Counter(word2)
+
+        sorted_values_word1 = sorted(frequency_word1.values())
+        sorted_values_word2 = sorted(frequency_word2.values())
+      
+        keys_match = set(frequency_word1.keys()) == set(frequency_word2.keys())
+
+        return sorted_values_word1 == sorted_values_word2 and keys_match
+
+'''
