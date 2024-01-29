a0d1b6da968fb16d9d4fa4e6718a2b3291c9d502
Park Jung Hoon <154233920+jhoonpark-codes@users.noreply.github.com>
2024-01-29 11:55:19 +0900

    Create 240129_232.py

---
+++ leetcode/daily_leetcode/240129_232.py
@@ -0,0 +1,55 @@
+'''
+232. Implement Queue using Stacks
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+'''
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = list()
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        p = self.stack[0]
+        self.stack = self.stack[1:]
+        return int(p)
+
+    def peek(self) -> int:
+        return int(self.stack[0])
+
+    def empty(self) -> bool:
+        if len(self.stack) == 0 :
+            return True
+        else:
+            return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
