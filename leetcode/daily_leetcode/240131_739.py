4e9b798791d1554fff876bdd25f6718b0cea78f5
Park Jung Hoon <154233920+jhoonpark-codes@users.noreply.github.com>
2024-01-31 18:24:38 +0900

    Update 240131_739.py

--- leetcode/daily_leetcode/240131_739.py
+++ leetcode/daily_leetcode/240131_739.py
@@ -44,4 +44,37 @@ class Solution:
                 else:
                     cnt += 1
         
-        return ans
+        return and
+
+
+
+'''
+best case 2
+from collections import deque
+
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures) - 1, -1, -1):
+            curr_temperature = temperatures[i]
+
+            while stack and curr_temperature >= temperatures[stack[-1]]:
+                stack.pop()
+
+            if stack:
+                result[i] = stack[-1] - i
+
+            stack.append(i)
+
+        return result
+
+
+'''
+
+
+
+
+
+
