eb4adc004038befe39a430ee423b5d5da15ca232
Park Jung Hoon <154233920+jhoonpark-codes@users.noreply.github.com>
2024-02-01 14:33:45 +0900

    Create 240201_2966.py

---
+++ leetcode/daily_leetcode/240201_2966.py
@@ -0,0 +1,74 @@
+'''
+2966. Divide Array Into Arrays With Max Difference
+
+You are given an integer array nums of size n and a positive integer k.
+
+Divide the array into one or more arrays of size 3 satisfying the following conditions:
+
+Each element of nums should be in exactly one array.
+The difference between any two elements in one array is less than or equal to k.
+Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,8,7,9,3,5,1], k = 2
+Output: [[1,1,3],[3,4,5],[7,8,9]]
+Explanation: We can divide the array into the following arrays: [1,1,3], [3,4,5] and [7,8,9].
+The difference between any two elements in each array is less than or equal to 2.
+Note that the order of elements is not important.
+Example 2:
+
+Input: nums = [1,3,3,2,7,3], k = 3
+Output: []
+Explanation: It is not possible to divide the array satisfying all the conditions.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+n is a multiple of 3.
+1 <= nums[i] <= 105
+1 <= k <= 105
+'''
+
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums = sorted(nums)
+        print('nums : ', nums)
+        
+        diffs = list()
+        idxs  = [0]
+        for i in range(len(nums)-1):
+            diff = nums[i+1] - nums[i]
+            diffs.append(diff)
+            if diff > k:
+                if i - idxs[-1] < 3:
+                    return []
+                else:
+                    idxs.append(i)
+        
+        if len(idxs) == 1:
+            split_points = [i for i in range(0, len(nums), 3)]
+            return [nums[ind:ind + 3] for ind in split_points]
+        
+        if len(nums) - idxs[-1] < 3:
+            return []
+        
+        idxs.append(len(nums)-1)
+        
+        print('diffs : ', diffs)
+        print('idxs  : ', idxs)
+        candidate = [nums[idxs[j] : idxs[j+1]] for j in range(len(idxs)-1)]
+        print('candidate : ', candidate)
+        
+        candidate_len = [len(tmp) for tmp in candidate]
+
+        if (1 in candidate_len) or (2 in candidate_len):
+            return []
+        else:
+            return candidate
+                
+        
